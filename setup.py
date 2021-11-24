fcc09827d84d1aead8b53e63a7fdedaa6b35d762
Gaetan Manchon <gmanchon@gmail.com>
2021-11-24 15:12:33 +0100

    add package author

--- setup.py
+++ setup.py
@@ -4,12 +4,14 @@ import os
 from setuptools import setup, find_packages
 
 with open("requirements.txt") as f:
-    content = f.readlines()
-requirements = [x.strip() for x in content]
+    requirements = [c.strip() for c in f.readlines()]
 
 setup(name="nbcleanmeta",
       version="0.2.0",
       description="Le Wagon notebook metadata cleaner tool",
+      url="https://github.com/lewagon/nbcleanmeta/",
+      author="Sébastien Saunier",
+      author_email="seb@lewagon.org",
       packages=find_packages(),
       install_requires=requirements,
       scripts=[os.path.join("scripts", "nbcleanmeta")])
