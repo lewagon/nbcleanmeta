d44b0e661ff5878f6dfc2dcf782b1035365c774f
Gaetan Manchon <gmanchon@gmail.com>
2022-02-16 12:53:16 +0100

    add tests for svg image cleaning

--- tests/test_cleaner_outputs.py
+++ tests/test_cleaner_outputs.py
@@ -32,3 +32,30 @@ class TestCleanerOutputs(TestCleanerBase):
             for output in cell.get('outputs', []):
                 if 'image/png' in output.get('data', {}):
                     assert output['data']['image/png'][-1:] != '\n'
+
+    def test_outputs_data_svg_image_is_multiline(self, notebook_path):
+        """
+        # test that image is stored multiline when no action is performed
+        """
+        # Act
+        # no action is performed
+
+        # Assert
+        clean_content = read_notebook(notebook_path)
+        code_cells = self._get_code_cells(clean_content)
+        for cell in code_cells:
+            for output in cell.get('outputs', []):
+                if 'image/svg+xml' in output.get('data', {}):
+                    assert len(output['data']['image/svg+xml']) > 1
+
+    def test_outputs_data_svg_image_is_inlined(self, notebook_path):
+        # Act
+        run_clean([notebook_path], False, False, {})
+
+        # Assert
+        clean_content = read_notebook(notebook_path)
+        code_cells = self._get_code_cells(clean_content)
+        for cell in code_cells:
+            for output in cell.get('outputs', []):
+                if 'image/svg+xml' in output.get('data', {}):
+                    assert len(output['data']['image/svg+xml']) == 1
